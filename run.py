36fbcf8cfeacaa59dde14c7bf6ec591ce162d597
R.Chabok <mail-to-bad-sector@proton.me>
2022-07-03 23:23:37 +0430

    Update run.py
    Add the ability to Reproduction, crossover and mutate.

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ import numpy as np
 from sys import exit
 flag = False
 crms = []
+counter = 0
 x = 0
+g_best = []  # gbest
 p_best = []  # pbest
 avg_fit_crm = []
 xi_end = 600
@@ -13,41 +15,103 @@ pop = 200
 generates = 1000
 end = 20
 x_save = []
+sum_all = 0 
+count = 0
+def start(xi):
+    global xi_end, x_save, crms
+    a = 1.0000 / 4000
+    xi = random.randrange(0, xi_end)
+    sums = 0
+    for j in range(1, 11):
+        sums += (xi * xi)
+    multi = cos(xi / sqrt(1)) + 1
+    for j in range(2, 11):
+        multi *= cos(xi / sqrt(j)) + 1
+    result = (a * sums) - multi
+    # avg += result
+    crms.append(result)
+    x_save.append(xi)
+    return(result)
+    # avg_fit_crm.append(avg / pop)
+    
 
+# def generate():
+#         global counter, pop, xi_end, crms, avg_fit_crm, select, x_save, x
+#         crms = []
+#         x_save = []
+#         x_save.append(x)
+#         crms.append(select)
+#         a = 1.0000 / 4000
+#         avg = 0
+#         for i in range(pop - 1):
+#             xi = random.randrange(0, xi_end)
+#             sum = 0
+#             for j in range(1, 11):
+#                 sum += (xi * xi)
+#             multi = cos(xi / sqrt(1)) + 1
+#             for j in range(2, 11):
+#                 multi *= cos(xi / sqrt(j)) + 1
+#             result = (a * sum) - multi
+#             avg += result
+#             crms.append(result)
+#             x_save.append(xi)
 
-def generate():
-        global pop, xi_end, crms, avg_fit_crm, select, x_save, x
-        crms = []
-        x_save = []
-        x_save.append(x)
-        crms.append(select)
-        a = 1.0000 / 4000
-        avg = 0
-        for i in range(pop - 1):
-            xi = random.randrange(0, xi_end)
-            sum = 0
-            for j in range(1, 11):
-                sum += (xi * xi)
-            multi = cos(xi / sqrt(1)) + 1
-            for j in range(2, 11):
-                multi *= cos(xi / sqrt(j)) + 1
-            result = (a * sum) - multi
-            avg += result
-            crms.append(result)
-            x_save.append(xi)
-
-        avg_fit_crm.append(avg / pop)
-        print("Generated.")
+#         avg_fit_crm.append(avg / pop)
+#         counter += 1
+#         print("{} Generated.".format(counter))
 
 def best_select():
-        global crms, p_best, select, x, x_save
+        global crms, select, x, x_save
+        x_save.append(x)
+        crms.append(select)
         crms_save = crms
         sorted_crms = sorted(crms_save)
-        p_best.append(sorted_crms[0])
-        select = sorted_crms[0]
         index = crms.index(sorted_crms[0])
         x = x_save[index]
 
+
+def crossover():
+    global x, x_save, crms, sum_all
+    a = random.randrange(-1*(len(x_save)-1),len(x_save)-1)
+    if a > 0:
+        print("We had a crossover in the {} generation".format(counter))
+        for i in range(a):
+            b = random.randrange(0,a)
+            sum_all -= crms[b]
+            x_save[b] = x_save[b] - x
+            crms[b] = start(x_save[b])
+            sum_all += crms[b]
+
+
+
+def mutation():
+    global x_save, crms, sum_all
+    copy_crms = crms
+    sorted_crms = sorted(copy_crms)
+    if sorted_crms[0]+sorted_crms[1] > sorted_crms[0]/2.0:
+        a = random.randrange(-1*(len(x_save)-1),len(x_save)-1)
+        if a > 0:
+            print("We had a mutation in the {} generation".format(counter))
+            for i in range(a):
+                b = random.randrange(0,a)
+                sum_all -= crms[b]
+                x_save[b]=x_save[b]/2.0
+                crms[b] = start(x_save[b])
+                sum_all += crms[b]
+
+def seter():
+    global crms, p_best, select, avg_fit_crm, x, sum_all, x_save, count
+    crms_save = crms
+    sorted_crms = sorted(crms_save)
+    p_best.append(sorted_crms[0])
+    select = sorted_crms[0]
+    index = crms.index(sorted_crms[0])
+    x = x_save[index]
+    avg_fit_crm.append(sum_all/count)
+    count = 0
+    sum_all = 0
+    crms = []
+    x_save = []
 
 
 def check():
@@ -76,20 +140,29 @@ def show():
 
 
 
+xi = random.randrange(0, 600)
+sums = 0
+for j in range(1, 11):
+    sums += (xi * xi)
+multi = cos(xi / sqrt(1)) + 1
+for j in range(2, 11):
+    multi *= cos(xi / sqrt(j)) + 1
+result = (1.000 / 4000 * sums) - multi
+select = result
+x = xi
 while True:
-    xi = random.randrange(0, 600)
-    sum = 0
-    for j in range(1, 11):
-        sum += (xi * xi)
-    multi = cos(xi / sqrt(1)) + 1
-    for j in range(2, 11):
-        multi *= cos(xi / sqrt(j)) + 1
-    result = (1.000 / 4000 * sum) - multi
-    select = result
     for i in range(generates):
-        generate()
+        for j in range(pop - 1):
+            start(random.randrange(0, 600))
+            count +=1
+        counter += 1
         best_select()
-        if i > 19:
+        crossover()
+        mutation()
+        seter()
+        
+        print("{} Generated.".format(counter))    
+        if i > 18:
             flag = check()
         if flag: break
     if flag:
